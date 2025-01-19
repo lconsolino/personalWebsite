71b3e2fb251b4e06cd4f07a326c6eee67c3d634d
Lizzie Consolino <lconsoli@usc.edu>
2025-01-19 13:51:32 -0800

    third commit

--- personalWebsiteServ.py
+++ personalWebsiteServ.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from waitress import serv
+from waitress import serve
 app = Flask(__name__)
 
 @app.route("/")
@@ -23,4 +23,4 @@ def sculpturePortfolio():
     return render_template('layout4.html')
 
 if __name__ == "__main__":
-    serv(app, host= "0.0.0.0", port=8000)
+    serve(app, host= "0.0.0.0", port=8000)
