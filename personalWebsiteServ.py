49776751e07bd0445ddd7e5f34d95e28335e6445
Lizzie Consolino <lconsoli@usc.edu>
2025-01-19 12:55:05 -0800

    first commit

---
+++ personalWebsiteServ.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('template1.html')
+
+@app.route("/template1")
+def back():
+    return render_template('template1.html')
+
+@app.route("/layout2")
+def compSciPortfolio():
+    return render_template('layout2.html')
+
+@app.route("/layout3")
+def compEngPortfolio():
+    return render_template('layout3.html')
+
+@app.route("/layout4")
+def sculpturePortfolio():
+    return render_template('layout4.html')
+
+if __name__ == "__main__":
+    app.run()
